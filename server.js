296aff6b5d2d0a4bd523cefb4355c667b0a8c6c5
Eitan Danino <eitann2000@gmail.com>
2023-01-08 21:59:51 +0200

    added config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
   console.log('UNHANDLED EXCPTION! Shutting down');
-  console.log(err.name, err.message, 'test');
+  console.log(err.name, err.message);
   process.exit(1);
 });
 
@@ -50,6 +50,13 @@ const server = app.listen(port, () =>
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! Shutting down');
-  console.log(err.name, err.message, 'test2');
+  console.log(err.name, err.message);
   server.close(() => process.exit(1));
+});
+
+process.on('SIGTERM', () => {
+  console.log(':) SIGTERM! Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated gracefully');
+  });
 });
