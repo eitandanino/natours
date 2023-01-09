be41c9504dfcafd7378992a77d7ed35a2e8a64b5
Eitan Danino <eitann2000@gmail.com>
2023-01-09 12:28:54 +0200

    Improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -66,6 +67,12 @@ const limiter = rateLimit({
   message: 'Too many requests, please try again in an hour.',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, Reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -10,16 +10,17 @@ router.get('/aboutUs', viewsController.getAboutUs);
 router.get('/contact', viewsController.getContactUs);
 router.get('/forgotPassword', viewsController.forgotPassword);
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
+router.get(
+  '/my-tours',
+  // bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
 
 router.post(
   '/submit-user-data',
