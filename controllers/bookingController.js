a700ad94df28c3310c9c9fbc7cece7baea27e0e5
Eitan Danino <eitann2000@gmail.com>
2023-01-09 12:46:40 +0200

    Improved stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -64,6 +64,7 @@ const createBookingCheckout = async (session) => {
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
   console.log({ tour, user, price });
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
