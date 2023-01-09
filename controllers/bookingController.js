84ac6afbdc65cce8efe03376a8814e62672facb1
Eitan Danino <eitann2000@gmail.com>
2023-01-09 12:46:09 +0200

    Improved stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -29,7 +29,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
         },
         quantity: 1,
@@ -59,6 +63,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
+  console.log({ tour, user, price });
   await Booking.create({ tour, user, price });
 };
 
